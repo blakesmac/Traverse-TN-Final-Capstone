03ee3a782e00e936ec7d1d960d1c80767462a08e
Blake Mcadams <blakemcadams@Blakes-MacBook-Air.local>
2022-03-15 21:33:21 -0500

    viewsetsandurlsMVP

--- traversetnapi/views/favorite.py
+++ traversetnapi/views/favorite.py
@@ -5,6 +5,7 @@ from rest_framework import serializers
 from rest_framework.response import Response
 from rest_framework.viewsets import ViewSet
 from traversetnapi.models import Favorite
+from traversetnapi.models import River, Place, Member
 
 class FavoriteView(ViewSet):
     
@@ -21,6 +22,41 @@ class FavoriteView(ViewSet):
             return Response(serializer.data)
         except Exception as ex:
             return HttpResponseServerError(ex)
+        
+        
+    def create(self, request, pk=None):
+        
+        user = request.auth.user.member
+        
+        river = River.objects.get(pk=request.data["riverId"])
+        place = Place.objects.get(pk=request.data["placeId"])
+        
+        
+        favorite = Favorite()
+        favorite.riverId = river
+        favorite.placeId = place
+        favorite.memberId = user
+        
+        try:
+            favorite.save()
+            serializer = FavoriteSerializer(favorite, context={'request': request})
+            return Response(serializer.data)
+        except ValidationError as ex:
+            return Response({"reason": ex.message}, status=status.HTTP_400_BAD_REQUEST)
+        
+    def destroy(self, request, pk=None):
+        try:
+            favorite = Favorite.objects.get(pk=pk)
+            favorite.delete()
+            
+            return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+        except Favorite.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+
+        except Exception as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
         
 class FavoriteSerializer(serializers.ModelSerializer):
     class Meta:
